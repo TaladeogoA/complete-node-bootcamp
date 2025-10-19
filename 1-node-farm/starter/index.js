670883f1c797f66918a4e9962ea58c71cb2b9c2e
TaladeogoA <ataladeogo@gmail.com>
2025-10-19 01:02:50 +0100

    Refactor node-farm project: - Implemented server functionality in index.js to serve product data and templates. - Added new HTML templates for product and overview pages. - Created a card template for displaying products. - Updated final.txt to separate content into two lines for clarity. - Added output.txt to store output data. - Removed old overview and product templates to streamline the project.

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+////////////////////////////////////////
+// FILES
+
+// Synchronous code
+// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textInput)
+
+// const textOutput = `We're outputting the same text: ${textInput}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOutput)
+// console.log('File written')
+
+// Asynchronous code - callback hell
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('err', err)
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
+//                 console.log('File written!')
+//             })
+//         })
+//     })
+// })
+
+
+////////////////////////////////////////
+// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+const overviewTemplate = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const cardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const productTemplate = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCT-NAME%}/g, product.productName);
+    output = output.replace(/{%PRODUCT-IMAGE%}/g, product.image);
+    output = output.replace(/{%PRODUCT-PRICE%}/g, product.price);
+    output = output.replace(/{%PRODUCT-ID%}/g, product.id);
+    output = output.replace(/{%PRODUCT-FROM%}/g, product.from);
+    output = output.replace(/{%PRODUCT-NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%PRODUCT-QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRODUCT-DESCRIPTION%}/g, product.description);
+
+    if (!product.organic) {
+        output = output.replace(/{%PRODUCT-NOT-ORGANIC%}/g, 'not-organic');
+    }
+    return output;
+}
+
+const server = http.createServer((req, res) => {
+    const { query, pathname } = url.parse(req.url, true);
+
+    // Overview page
+    if (pathname === '/overview' || pathname === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        const htmlCards = dataObj.map(product => replaceTemplate(cardTemplate, product)).join('')
+        const output = overviewTemplate.replace('{%PRODUCT-CARDS%}', htmlCards)
+        res.end(output)
+
+    // Product page
+    } else if (pathname === '/product') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        const product = dataObj[query.id]
+        const output = replaceTemplate(productTemplate, product)
+        res.end(output)
+
+    // API page
+    } else if (pathname === '/api') {
+        res.writeHead(200, { 'content-type': 'application/json' })
+        res.end(data)
+
+    // 404
+    } else {
+        res.writeHead(404)
+        res.end('Page not found.')
+    }
+})
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server listening on port 8000')
+})
